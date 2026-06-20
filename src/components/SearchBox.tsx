ec263913f7486af236729f4205665e0b02d122e5
DHANASEKAR <71744@TCS.Local>
2026-06-20 16:24:58 +0530

    debounce search added

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { useState, type ReactNode } from "react";
+import { useEffect, useMemo, useState, type ReactNode } from "react";
 import { AutoComplete, ConfigProvider, Input } from "antd";
 import { CircleX, Search } from "lucide-react";
 import { Spin } from "antd";
@@ -7,6 +7,8 @@ import { useLazyFetchUsersQuery } from "../store/services/userApi";
 import { useCreateChatMutation } from "../store/services/chatApi";
 import { errorToastHandler } from "../utils/toastHandler";
 import { Avatar } from "../utils/getAvatar";
+import debounce from 'lodash.debounce'
+
 
 interface Options {
     value: string,
@@ -18,21 +20,17 @@ export default function SearchBox() {
     const [value, setValue] = useState("");
     const [options, setOptions] = useState<Options[] | null>(null);
     const [loading, setLoading] = useState(false);
-    const [ fetchUsers ] = useLazyFetchUsersQuery()
-    const [ createChat ] = useCreateChatMutation()
+    const [fetchUsers] = useLazyFetchUsersQuery()
+    const [createChat] = useCreateChatMutation()
 
 
     const makeUsersList = (users: UserInterface[]) => {
         return users?.map((e) => ({
-            value: e.user_id,
+            value: e.name,
+            user_id: e.user_id,
             label: (
                 <div className="flex items-center gap-3 py-2 font-sn-pro">
                     <Avatar name={e.name} image={e.image} className="size-8!" />
-                    {/* <img
-                        src={e.image}
-                        alt=""
-                        className="w-8 h-8 rounded-full"
-                    /> */}
                     <div>
                         <div className="font-medium capitalize">{e.name}</div>
                         <div className="text-xs text-gray-500">
@@ -44,29 +42,39 @@ export default function SearchBox() {
         }))
     }
 
-    const handleFetchUsers = async (searchText: string) => {
-        if (!searchText.trim()) {
-            setOptions(null);
-            return;
-        }
+    const handleFetchUsers = useMemo(() =>
+        debounce(async (searchText: string) => {
 
-        try {
-            setLoading(true);
+            if (!searchText.trim()) {
+                setOptions(null);
+                return;
+            }
 
-            const result = await fetchUsers({ search: searchText.trim() }).unwrap()
+            try {
+                setLoading(true);
 
-            const users = makeUsersList(result)
+                const result = await fetchUsers({ search: searchText.trim() }).unwrap()
 
-            setOptions(users);
-        }
-        catch (error) {
-            errorToastHandler(error)
-            setOptions(null);
-        }
-        finally {
-            setLoading(false);
-        }
-    };
+                const users = makeUsersList(result)
+
+                setOptions(users);
+            }
+            catch (error) {
+                errorToastHandler(error)
+                setOptions(null);
+            }
+            finally {
+                setLoading(false);
+            }
+
+        }, 500)
+    , [fetchUsers])
+
+    useEffect(() => {
+        return () => {
+            handleFetchUsers.cancel();
+        };
+    }, [handleFetchUsers]);
 
     const handleChange = (text: string) => {
         setValue(text);
@@ -100,7 +108,11 @@ export default function SearchBox() {
                 options={options ?? undefined}
                 style={{ width: 400 }}
                 onChange={handleChange}
-                onSelect={(selectedValue) => { handleCreateChat(selectedValue) }}
+                onSelect={(_, option: any) => {
+                    handleCreateChat(option.user_id);
+                    setValue('');
+                    setOptions([])
+                }}
                 allowClear={{
                     clearIcon: (
                         <CircleX
